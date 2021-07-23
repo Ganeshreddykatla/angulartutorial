b6d2716e8b4304940ff4a9746d4beabe5ab1fa65
Ganeshreddykatla <ganeshreddykatla@gmail.com>
2021-07-23 07:18:39 +0530

    angular3rd class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { TestModule} from './test/test.module';
+import { UserComponent } from './user/user.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-   
+    TestModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
